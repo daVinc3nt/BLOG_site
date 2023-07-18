6ad92a73292d5a9c813503e87a5b170b913aba80
phamquangviet891 <20522153@gm.uit.edu.vn>
2023-07-18 17:32:26 +0700

    Add button light/dark mode done

--- data/headerNavLinks.js
+++ data/headerNavLinks.js
@@ -1,7 +1,8 @@
 const headerNavLinks = [
-  { href: '/blog', title: 'Trang chủ' },
-  { href: '/tags', title: 'Bài viết' },
-  { href: '/projects', title: 'Hỏi đáp' },
+  { href: '/', title: 'Trang chủ' },
+  { href: '/blog', title: 'Bài viết' },
+  { href: '/tags', title: 'Kiến thức' },
+  { href: '/', title: 'Hỏi đáp' }, // lẽ ra là /projects nhưng tạm thời xóa đã
   { href: '/about', title: 'Liên hệ' },
 ]
 
